28c1671fac74df3aeca4574199cbf56663f3102a
Harsh <harshmaan028@gmail.com>
2026-06-17 01:24:53 +0530

    Fix Tailwind and multipage build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  plugins: [tailwindcss()],
+
   build: {
     rollupOptions: {
       input: {
